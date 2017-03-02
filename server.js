08fbfb86037d38912f6fce936beaf6b68d77c0ea
pparonson <preston.aronson@gmail.com>
2017-03-02 07:14:24 -0700

    fix prep heroku edits in app.use

--- server.js
+++ server.js
@@ -2,21 +2,23 @@ const express = require('express');
 
 // create the app
 const app = express();
+// configure port with environment var for heroku or 3000 for localhost
+const PORT = process.env.PORT || 3000;
 
-// express middleware
+// express middleware pattern to do something with requests
 app.use((req, res, next) => {
-  // openWeatherMap does NOT accept https, thus route to http
   if (req.headers['x-forwarded-proto'] === 'http') {
+    console.log(req.headers['x-forwarded-proto']);
     next();
   } else {
-    res.redirect(`http://${req.hostname}${req.url}`);
+    console.log(`hostname: ${req.hostname}, url: ${req.url}`);
   }
 });
+
 app.use(express.static('public'));
 
 
-// configure port with environment var for heroku or 3000 for localhost
-const PORT = process.env.PORT || 3000;
-app.listen(port, () => {
+
+app.listen(PORT, () => {
   console.log(`Express server is running on port: ${PORT}`);
 });
