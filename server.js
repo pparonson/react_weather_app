63820cd719cf49bb87b42fbf62c196cca64d5f46
pparonson <preston.aronson@gmail.com>
2017-03-02 05:33:47 -0700

    prep repo for heroku

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const express = require('express');
 // create the app
 const app = express();
 
+// express middleware
+app.use((req, res, next) => {
+  // openWeatherMap does NOT accept https, thus route to http
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect(`http://${req.hostname}${req.url}`);
+  }
+});
 app.use(express.static('public'));
 
-const port = 3000;
+
+// configure port with environment var for heroku or 3000 for localhost
+const PORT = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Express server is running on port: ${port}`);
+  console.log(`Express server is running on port: ${PORT}`);
 });
