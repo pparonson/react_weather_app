ce1e6aca053e8c603b061c0bb1d82609a876eafa
pparonson <preston.aronson@gmail.com>
2017-03-02 07:36:32 -0700

    remove console.logs from express middleware app.use

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const express = require('express');
 const app = express();
 // configure port with environment var for heroku or 3000 for localhost
 const PORT = process.env.PORT || 3000;
-console.log(process.env.PORT);
 
 // express middleware pattern to do something with requests
 app.use((req, res, next) => {
   if (req.headers['x-forwarded-proto'] === 'http') {
-    console.log(req.headers['x-forwarded-proto']);
     next();
   } else {
-    console.log(`hostname: ${req.hostname}, url: ${req.url}`);
     res.redirect(`http://${req.hostname}${req.url}`)
   }
 });
